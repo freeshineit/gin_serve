8e9228fca054e704370c25715a7405cd6e30eb53
ShineShao <xiaoshaoqq@gmail.com>
2023-01-20 16:09:36 +0800

    feat: merge map

---
+++ utils/map.go
@@ -0,0 +1,19 @@
+package utils
+
+import "fmt"
+
+// merge map
+func MergeMap[K comparable, V any](mObj ...map[K]V) map[K]V {
+
+	newObj := make(map[K]V)
+
+	for _, m := range mObj {
+		for k, v := range m {
+			newObj[k] = v
+		}
+	}
+
+	fmt.Println(newObj)
+
+	return newObj
+}
