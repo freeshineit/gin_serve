f818d7863db97fd7e5667998345eac58ca828bf8
ShineShao <xiaoshaoqq@gmail.com>
2023-01-31 15:24:35 +0800

    feat: new todo status type

--- app/models/todo.go
+++ app/models/todo.go
@@ -1,12 +1,16 @@
 package models
 
+type Todo_Status_Type int
+
 const (
-	Todo_STATUS_DEFAULT int = 0
-	Todo_STATUS_CHECKED int = 1
+	// 默认
+	Todo_Status_Default Todo_Status_Type = 0
+	// 选中
+	Todo_Status_Checked Todo_Status_Type = 1
 )
 
 type Todo struct {
-	Id      string `json:"id" form:"id"`
-	Status  int    `json:"status" form:"status"`
-	Content string `json:"content" form:"content" binding:"required"`
+	Id      string           `json:"id" form:"id"`
+	Status  Todo_Status_Type `json:"status" form:"status"`
+	Content string           `json:"content" form:"content" binding:"required"`
 }
