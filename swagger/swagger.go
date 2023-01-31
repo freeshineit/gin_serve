4cba2a1b3a4a7b76e2e6ec82a820e85f8c8e871d
ShineShao <xiaoshaoqq@gmail.com>
2023-01-31 22:55:50 +0800

    update

--- swagger/swagger.go
+++ swagger/swagger.go
@@ -9,6 +9,12 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 // InitSwagger init swagger docs
 func InitSwagger(r *gin.Engine) {
 
@@ -17,7 +23,6 @@ func InitSwagger(r *gin.Engine) {
 	url := ginSwagger.URL("http://localhost:8080/docs/swagger.json") // The url pointing to API definition
 
 	log.Printf("swagger docs api url %s \n", helpers.ColorBlueString("http://localhost:8080/swagger/index.html"))
-
 	// http://localhost:8080/swagger/index.html
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 }
