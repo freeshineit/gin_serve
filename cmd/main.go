8ad2c3ef276eac2385256467c8cbfe077046a2bd
ShineShao <xiaoshaoqq@gmail.com>
2023-02-04 16:25:49 +0800

    update swagger docs

--- cmd/main.go
+++ cmd/main.go
@@ -71,7 +71,23 @@ func initConfig() {
 	// log.Printf("Load config success ...")
 }
 
-// 执行
+// @title           Gin Serve Api
+// @version         0.1.0
+// @description     This is a sample server celler server.
+// @termsOfService  https://github.com/freeshineit/gin_serve
+
+// @contact.name   API Support
+// @contact.url    https://github.com/freeshineit/gin_serve
+// @contact.email  xiaoshaoqq@gmail.com
+
+// @license.name  MIT
+// @license.url   https://github.com/freeshineit/gin_serve/blob/main/LICENSE
+
+// @host      localhost:8080
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @BasePath /api
 func main() {
 	execute()
 }
--- swagger/swagger.go
+++ swagger/swagger.go
@@ -9,21 +9,6 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-// @title           Gin Serve Api
-// @version         1.0
-// @description     This is a sample server celler server.
-// @termsOfService  http://swagger.io/terms/
-
-// @contact.name   API Support
-// @contact.url    http://www.swagger.io/support
-// @contact.email  support@swagger.io
-
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host      localhost:8080
-// @BasePath  /api
-// @securityDefinitions.basic  BasicAuth
 func InitSwagger(r *gin.Engine) {
 
 	r.Static("/docs", "./docs")
