61267dcdb535e4c8398dfdda67fb250182876dd4
ShineShao <xiaoshaoqq@gmail.com>
2023-02-05 14:30:14 +0800

    Fix tage

--- app/model/user.go
+++ app/model/user.go
@@ -8,8 +8,7 @@ type User struct {
 	Name     string `json:"name" form:"name" binding:"required" gorm:"type:varchar(255)"`
 	Email    string `json:"email" form:"email" binding:"required" gorm:"uniqueIndex;type:varchar(255)"`
 	Age      uint   `json:"age" form:"age" binding:"required" gorm:"type:int"`
-	Gender   string `json:"gender" form:"gender" binding:"required" gorm:"type:int"`
+	Gender   string `json:"gender" form:"gender" binding:"required" gorm:"type:varchar(40)"`
 	Avatar   string `json:"avatar" form:"avatar" binding:"required" gorm:"type:varchar(255)"`
 	Password string `json:"-" gorm:"->;<-;not null"`
-	// Token    string `json:"token,omitempty" from: "token,omitempty" gorm:"-"`
 }
