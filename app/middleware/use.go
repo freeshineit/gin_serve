4efcf23b2273f61f02dace33c4b117665db51278
ShineShao <xiaoshaoqq@gmail.com>
2023-02-13 01:35:38 +0800

    update cors

--- app/middleware/use.go
+++ app/middleware/use.go
@@ -1,12 +1,22 @@
 package middleware
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetMiddleware(router *gin.Engine) {
-	// cors
-	Cors(router)
-	// jwt
-
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 }
