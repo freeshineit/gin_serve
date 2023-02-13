f0aa7baa1e874e4612a6332b30a22f8d6488656a
ShineShao <xiaoshaoqq@gmail.com>
2023-02-13 22:59:15 +0800

    add note Why does signal.Notify use buffered channel

--- helper/server.go
+++ helper/server.go
@@ -24,6 +24,7 @@ func ListenAndServe(srv *http.Server) error {
 
 	// Wait for interrupt signal to gracefully shutdown the server with
 	// a timeout of 5 seconds.
+	// https://www.sobyte.net/post/2022-06/signal-channel/
 	quit := make(chan os.Signal, 1)
 	// kill (no param) default send syscall.SIGTERM
 	// kill -2 is syscall.SIGINT
